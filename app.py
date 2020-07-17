b31732ee392f735cdb02d28add135f48aadb6711
Juan Sebastian Galindo <Sebastian@Juans-MBP.attlocal.net>
2020-07-16 19:40:29 -0500

    Added close session

--- app.py
+++ app.py
@@ -25,8 +25,6 @@ measurement = Base.classes.measurement
 
 station = Base.classes.station
 
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -41,16 +39,20 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        "<ul>"
+        f"<li>Available Routes:<br/></li>"
+        f"<li>/api/v1.0/precipitation<br/></li>"
+        f"<li>/api/v1.0/stations<br/></li>"
+        f"<li>/api/v1.0/tobs<br/></li>"
+        f"<li>/api/v1.0/<start><br/></li>"
+        f"<li>/api/v1.0/<start>/<end><br/></li>"
+        "</ul>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session(engine)
+
     #Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
 
     precipitation_data = session.query(measurement.date, measurement.prcp).order_by(measurement.date).all() 
@@ -64,7 +66,8 @@ def precipitation():
 
         else:
             precipitation_dict[date].append(prcp)
-
+            
+    session.close()
     #Return the JSON representation of your dictionary.
     precipitation_dict
 
@@ -72,23 +75,29 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session(engine)
+
     #Return a JSON list of stations from the dataset.
 
     stationsActivity = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
-
+    
+    session.close()
     stationsList = [i[0] for i in stationsActivity]
 
     return jsonify(stationsList)
 
 @app.route("/api/v1.0/tobs")
 def temperature():
+    session = Session(engine)
+
     #Query the dates and temperature observations of the most active station for the last year of data.
 
     startingDate = (pd.to_datetime(list(session.query(measurement.date).all())[-1][0]) - dt.timedelta(days = 365)).strftime("%Y-%m-%d")
     mostActiveStation = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()[0][0]
 
     twelveMonthDataTempStation = session.query(measurement.date, measurement.tobs).filter(measurement.date >= startingDate).filter(measurement.station == mostActiveStation).order_by(measurement.date).all()
-
+    
+    session.close()
     #Return a JSON list of temperature observations (TOBS) for the previous year.
     tempList = [i[1] for i in twelveMonthDataTempStation]
 
@@ -98,20 +107,26 @@ def temperature():
 
 @app.route("/api/v1.0/<start>") #yyyy-month-day
 def tempStartRange(start):
+    session = Session(engine)
+
     #When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
 
     tempStartRange_func = session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)).filter(measurement.date >= start).all()
-
+    
+    session.close()
     tempStartRange_list = list(np.ravel(tempStartRange_func))
 
     return jsonify(tempStartRange_list)
 
 @app.route("/api/v1.0/<start>/<end>") #yyyy-month-day
 def tempStartEndRange(start,end):
+    session = Session(engine)
+
     #When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
 
     tempStartEndRange_func = session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).all()
-
+    
+    session.close()
     tempStartEndRange_list = list(np.ravel(tempStartEndRange_func))
 
     return jsonify(tempStartEndRange_list)
